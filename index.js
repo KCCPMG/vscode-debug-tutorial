1ff52a1c5da5a20263f29287d725234f99476d70
Connor Wales <connorwales@gmail.com>
2024-09-01 17:07:14 -0700

    change breakpoints to calls for 'debugger', add .vscode/launch.json

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("beginning of index.js used for debug test");
+
+const obj = {
+  a: 0,
+  b: 1,
+  c: 2
+}
+
+// without the debugger line below, a breakpoint here would go to the next line of code, the for loop
+debugger;
+
+for (let i=1; i<5; i++) {
+  obj.a = (obj.a * i) + i;
+  obj.b = (obj.b * i) + i;
+  obj.c = (obj.c * i) + i;
+
+  debugger;
+}
+
+console.log({obj});
+console.log("end of index.js")
